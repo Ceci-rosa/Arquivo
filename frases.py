a2542b057826641797ca5b2044500572d6c5360c
Ceci-rosa <sanaceciliarosade@gmail.com>
2025-06-06 07:47:29 -0300

    .

---
+++ frases.py
@@ -0,0 +1,6 @@
+with open("frases.txt", "w") as f:
+    f.write("Hoje o dia está bonito\n")
+    f.write("Vou estudar Python\n")
+    f.write("Arquivos são legais de usar\n")
+
+print("Arquivo 'frases.txt' criado com sucesso!")
